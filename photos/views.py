31728089429b5504e9a6bcbba540f7248d65cf25
kapphire <ken.sapphire87@yahoo.com>
2018-04-07 05:39:15 +1200

    ipaddress checker added

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,4 @@
+import ipaddress
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -8,15 +9,34 @@ from .forms import PhotoUploadForm
 from .models import Photo
 from .settings import SHOW_PER_PAGE
 
+
+def get_ip(request):
+    try:
+        x_forward = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forward:
+            ip = x_forward.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+    except:
+        ip = ""
+    return ip
+
 def home(request):
     """Displays all uploaded images"""
     # Sorted by most recently uploaded.
+    client_ip = get_ip(request)
+    if ipaddress.ip_address(client_ip).__class__ is ipaddress.IPv6Address:
+        msg = 'You are great!'
+        ip_valid = True
+    else:
+        msg = 'Oh, sorry...'
+        ip_valid = False
     photos = Photo.objects.all().order_by('-uploaded_at')
     paginator = Paginator(photos, SHOW_PER_PAGE)
     page = request.GET.get('page')
     paginated_photos = paginator.get_page(page)
 
-    return render(request, 'photos/home.html', {'paginated_photos': paginated_photos})
+    return render(request, 'photos/home.html', {'paginated_photos': paginated_photos, 'msg': msg, 'ip_valid': ip_valid})
 
 def detail(request, photo_id):
     """View a single photo"""
